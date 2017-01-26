cd40fd106f986b5c7b2d866f3911b24db526eb04
isfon <lamgers@gmail.com>
2017-01-26 09:46:24 -0600

    se cambio el menu por componente

--- app/scripts/controllers/home/home.js
+++ app/scripts/controllers/home/home.js
@@ -4,16 +4,9 @@ angular
 	.controller('HomeCtrl', function($localStorage, $location) {
 		function initialData() {
 			vm.menus = $localStorage.currentUser.Menu;
-			vm.usuario = $localStorage.currentUser.usuario;
 			console.log(vm.menus);
 		}
 
-		function logout() {
-			delete $localStorage.currentUser;
-			$location.path('/auth/');
-		}
-
 		var vm = this;
-		vm.logout = logout;
 		initialData();
 	});
