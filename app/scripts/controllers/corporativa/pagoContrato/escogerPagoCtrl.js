4b0af50ce84bced3a9bd8f9f572a70d33a478ac2
Eduardo González <laloe.90@gmail.com>
2017-03-31 19:02:42 -0600

    termino de pagos contado y credito

--- app/scripts/controllers/corporativa/pagoContrato/escogerPagoCtrl.js
+++ app/scripts/controllers/corporativa/pagoContrato/escogerPagoCtrl.js
@@ -82,6 +82,73 @@ function EscogerPagoCtrl($uibModal, $state, $rootScope, cajasFactory, ngNotify, 
 		}
 	}
 
+	function guardarVariable() {
+		if (vm.abono == undefined || vm.abono == null || vm.abono == 0 || vm.abono < 0) {
+			ngNotify.set('Los campos no deben de ir nulos, negativos o en 0', 'error');
+		}else {
+			var objPagar = {
+				'contrato': items.Contrato,
+				'credito':metodo,
+				'cajera': $localStorage.currentUser.usuario,
+				'maquina': $localStorage.currentUser.maquina,
+				'sucursal': $localStorage.currentUser.sucursal,
+				'compania': items.Compania,
+				'distribuidor': items.Distribuidor,
+				'sessionPadre': items.SessionPadre,
+				'tipo': 0,
+				'monto': vm.monto,
+				'GLOEFECTIVO2': 0,
+				'GLOCHEQUE2': 0,
+				'GLOCLV_BANCOCHEQUE2': 0,
+				'NUMEROCHEQUE2': '',
+				'GLOTARJETA2': 0,
+				'GLOCLV_BANCOTARJETA2': 0,
+				'NUMEROTARJETA2': '',
+				'TARJETAAUTORIZACION2': '',
+				'CLV_Nota2': 0,
+				'GLONOTA3': 0,
+				'token': $localStorage.currentUser.token1,
+				'NoPagos' : 0,
+				'PagoInicial': vm.abono
+			};
+			console.log(objPagar);
+			pagosMaestrosFactory.grabaFactura(objPagar).then(function(dataGraba) {
+				console.log(dataGraba);
+				vm.clvCactura = dataGraba.GetGrabaFacturaCMaestroResult.ClvFacturaMaestro;
+				var elem = {
+					'NoPagos' : 0,
+					'PagoInicial': vm.abono,
+					'Clv_FacturaMaestro': vm.clvCactura
+				};
+				console.log(elem);
+				$uibModalInstance.dismiss('cancel');
+				vm.animationsEnabled = true;
+				var modalInstance = $uibModal.open({
+					animation: vm.animationsEnabled,
+					ariaLabelledBy: 'modal-title',
+					ariaDescribedBy: 'modal-body',
+					templateUrl: 'views/corporativa/pagarCredito.html',
+					controller: 'PagarCreditoCtrl',
+					controllerAs: '$ctrl',
+					backdrop: 'static',
+					keyboard: false,
+					size: 'md',
+					resolve: {
+						items: function() {
+							return items;
+						},
+						metodo: function() {
+							return metodo;
+						},
+						elem: function() {
+							return elem;
+						}
+					}
+				});
+			});
+		}
+	}
+
 	function operacion() {
 		if (vm.pagoInicial > vm.monto) {
 			vm.pagoInicial = 0;
@@ -102,6 +169,7 @@ function EscogerPagoCtrl($uibModal, $state, $rootScope, cajasFactory, ngNotify, 
     var vm = this;
     vm.cambio = cambio;
 	vm.guardarFijo = guardarFijo;
+	vm.guardarVariable = guardarVariable;
 	vm.operacion = operacion;
     vm.cancel = cancel;
 	vm.monto = items.Monto;
