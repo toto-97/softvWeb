1f5d2f5af01dc3820962de1fa89c2569bc2ef999
EricChavez <erictics@gmail.com>
2017-05-10 19:01:54 -0500

    cambios template

--- app/scripts/controllers/corporativa/NotaCredito/nuevaNotaCreditoCtrl.js
+++ app/scripts/controllers/corporativa/NotaCredito/nuevaNotaCreditoCtrl.js
@@ -3,5 +3,30 @@ angular.module('softvApp').controller('nuevaNotaCreditoCtrl', nuevaNotaCreditoCt
 
 function nuevaNotaCreditoCtrl($uibModal, $state, $rootScope, ngNotify) {
 
-var vm=this;
+
+  function abrirContratos() {
+
+    var modalInstance = $uibModal.open({
+      animation: true,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'views/corporativa/ModalBuscaContratoMaestro.html',
+      controller: 'ModalBusquedaCMCtrl',
+      controllerAs: '$ctrl',
+      backdrop: 'static',
+      keyboard: false,
+      size: "lg",
+      resolve: {}
+    });
+  }
+
+  $rootScope.$on('contrato_nota', function (e, contrato) {
+    console.log(contrato);
+    vm.DetalleContrato=contrato;
+    vm.Contrato = contrato.IdContratoMaestro;
+  });
+
+
+  var vm = this;
+  vm.abrirContratos = abrirContratos;
 }
