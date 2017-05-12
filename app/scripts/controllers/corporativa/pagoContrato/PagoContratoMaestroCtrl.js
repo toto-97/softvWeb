b0a11d6e89632901aca66bd81ca43efe15f655bb
Eduardo González <laloe.90@gmail.com>
2017-05-11 19:00:18 -0500

    pagos contratos maestros

--- app/scripts/controllers/corporativa/pagoContrato/PagoContratoMaestroCtrl.js
+++ app/scripts/controllers/corporativa/pagoContrato/PagoContratoMaestroCtrl.js
@@ -43,25 +43,19 @@ function PagoContratoMaestroCtrl($uibModal, $state, $rootScope, cajasFactory, ng
             } else {
                 vm.muestraCliente = true;
                 pagosMaestrosFactory.cobraSaldoMaestro(vm.Contratos.IdContratoMaestro).then(function (data) {
-
-                    vm.saldo = data.GetDeepCobraSaldoContratoMaestroResult;
-                    cajasFactory.dameDetallePago(vm.saldo.ClvSession).then(function (detallePago) {
-                        if (detallePago.GetDameDetalleListResult.length == 0) {
+                    vm.saldo = data.GetCobraContratoMaestroResult;
+                    pagosMaestrosFactory.dameDetalle(vm.saldo.Clv_SessionPadre).then(function (detallePago) {
+                        if (detallePago.GetDetalleContratosMaestrosListResult.length == 0) {
                             vm.blockBaja = true;
                             vm.blockPagar = true;
                         } else {
                             vm.blockBaja = false;
                             vm.blockPagar = false;
                         }
-                        vm.detallePago = detallePago.GetDameDetalleListResult;
+                        vm.detallePago = detallePago.GetDetalleContratosMaestrosListResult.lista;
+                        vm.sumaPagos = detallePago.GetDetalleContratosMaestrosListResult.datosdetalle;
                         vm.detallePagoAux = vm.detallePago;
                     });
-                    cajasFactory.dameSumaPago(vm.saldo.ClvSession).then(function (sumaPago) {
-                        vm.sumaPagos = sumaPago.GetSumaDetalleListResult;
-                    });
-                    // cajasFactory.obtenEdoCuenta(vm.Contratos.IdContratoMaestro,vm.saldo.ClvSession).then(function(data) {
-                    //     vm.edoCuenta = data.GetObtieneEdoCuentaSinSaldarListResult;
-                    // });
                 });
                 resetBusquedas();
                 $('.datosCliente').collapse('show');
@@ -284,6 +278,33 @@ function PagoContratoMaestroCtrl($uibModal, $state, $rootScope, cajasFactory, ng
         });
     }
 
+    function abrirDetalle() {
+        // var items = {
+        //     Contrato: vm.Contratos.IdContratoMaestro,
+        //     Compania: vm.saldo.IdCompania,
+        //     Distribuidor: vm.saldo.IdDistribuidor,
+        //     Session: vm.saldo.ClvSession,
+        //     SessionPadre: vm.saldo.ClvSessionPadre,
+        //     Monto: data.GetDeepSumaTotalDetalleResult.Monto
+        // };
+        vm.animationsEnabled = true;
+        var modalInstance = $uibModal.open({
+            animation: vm.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'views/corporativa/abrirDetalle.html',
+            controller: 'AbrirDetalleCtrl',
+            controllerAs: '$ctrl',
+            backdrop: 'static',
+            keyboard: false,
+            size: 'lg'
+            // resolve: {
+            //     items: function () {
+            //         return items;
+            //     }
+            // }
+        });
+    }
 
     var vm = this;
     $('.buscarContrato').collapse();
