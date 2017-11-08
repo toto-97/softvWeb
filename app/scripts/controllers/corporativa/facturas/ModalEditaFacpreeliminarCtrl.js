9a2618a8bc7df15391bf09616b2e22e2ca537e3c
EricChavez <erictics@gmail.com>
2017-11-08 12:12:27 -0600

    se integro la edicion y agregacion de conceptos en una refactura

--- app/scripts/controllers/corporativa/facturas/ModalEditaFacpreeliminarCtrl.js
+++ app/scripts/controllers/corporativa/facturas/ModalEditaFacpreeliminarCtrl.js
@@ -14,40 +14,71 @@
     vm.opcion = 0; //1--guardar conceptos 2--facturar -3.-refacturar
     vm.total = 0;
     vm.calcularmonto = calcularmonto;
+    vm.deleteItem=deleteItem;
+    vm.AddItem=AddItem;
+    vm.cambioDescripcion=cambioDescripcion;
+    vm.cambioMonto=cambioMonto;
+
     this.$onInit = function () {
       vm.clave = obj.clave;
       vm.status = obj.status;
       ContratoMaestroFactory.DameDetalle_FacturaMaestroFiscal(vm.clave).then(function (data) {
         vm.conceptos = data.GetDameDetalle_FacturaMaestroFiscalListResult;
         vm.conceptos.forEach(function (item) {
-          vm.total = vm.total + item.Importe;
+          console.log(item);
+          item.Accion=1;
+          vm.total = vm.total + item.Importe;          
         });
       });
     };
 
-
     function cancel() {
       $uibModalInstance.dismiss('cancel');
-
     }
 
+    function AddItem(){
+      var concepto={};
+      concepto.Accion=0;
+      concepto.Clv_Detalle=0;
+      concepto.Clv_FacturaMaestro=obj.clave;
+      concepto.Descripcion=vm.conceptonuevo;
+      concepto.Importe=vm.montonuevo;
+      vm.conceptos.push(concepto)
+      calcularmonto();
+      vm.conceptonuevo='';
+      vm.montonuevo=0;
+    }
+
+    function deleteItem(index){     
+     vm.conceptos[index].Accion=2;
+     calcularmonto();
+    }
+
+    function cambioMonto(imp,index){
+      console.log(imp,index);
+      vm.conceptos[index].Importe=imp;
+      calcularmonto();
+    }
+
+    function cambioDescripcion(des,index){
+      vm.conceptos[index].Descripcion=des;      
+    }
 
     function calcularmonto() {
       vm.total = 0;
       vm.conceptos.forEach(function (item) {
-        vm.total += (item.Importe === undefined) ? 0 : item.Importe
-
+        vm.total += (item.Importe === undefined || item.Accion ===2 ) ? 0 : item.Importe;
       });
     }
 
     function ok() {
-
       var array_ = [];
       vm.conceptos.forEach(function (item) {
         var obj = {};
         obj.Clv_Detalle = item.Clv_Detalle;
         obj.Descripcion = item.Descripcion;
         obj.Importe = item.Importe;
+        obj.Accion=item.Accion;
         array_.push(obj);
       });
       var tipo = (vm.status === 'Por Facturar') ? 1 : 3;
