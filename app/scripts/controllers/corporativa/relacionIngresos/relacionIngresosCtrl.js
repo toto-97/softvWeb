283f09a799f268bb049b070b78fb70f8e15a7c95
unknown <jano_ea@hotmail.com>
2018-03-20 13:15:51 -0600

    Correciones Polizas

--- app/scripts/controllers/corporativa/relacionIngresos/relacionIngresosCtrl.js
+++ app/scripts/controllers/corporativa/relacionIngresos/relacionIngresosCtrl.js
@@ -6,7 +6,7 @@ function relacionIngresosCtrl(ContratoMaestroFactory, relacionIngresosFactory,$f
 
 
   this.$onInit = function () {
-    ContratoMaestroFactory.GetDistribuidores().then(function (result) {
+    /*ContratoMaestroFactory.GetDistribuidores().then(function (result) {
       console.log(result);
       vm.options = {
         filterPlaceHolder: 'filtrar distribuidor',
@@ -17,7 +17,7 @@ function relacionIngresosCtrl(ContratoMaestroFactory, relacionIngresosFactory,$f
         items: result.GetDistribuidoresResult,
         selectedItems: []
       }
-    });
+    });*/
 
 
 
@@ -30,12 +30,13 @@ function relacionIngresosCtrl(ContratoMaestroFactory, relacionIngresosFactory,$f
         var fechainicio=$filter('date')(vm.fechainicio, 'yyyy/MM/dd');
         var fechafin=$filter('date')(vm.fechafin, 'yyyy/MM/dd');
         var dolares=(vm.dolares==true)?1:0;
-        vm.options.selectedItems.forEach(function(item){ dis.push(item.Clv_Plaza)});       
-            
-      relacionIngresosFactory.GetRelacionIngresosMaestro(dis,fechainicio,fechafin,dolares).then(function (result) { 
-       console.log(result);
+        //vm.options.selectedItems.forEach(function(item){ dis.push(item.Clv_Plaza)});       
+        //dis.push(item.Clv_Plaza);
+
+        relacionIngresosFactory.GetRelacionIngresosMaestro(dis,fechainicio,fechafin,dolares).then(function (result) { 
+        console.log(result);
         console.log(globalService.getUrlReportes()+"/Reportes/"+result.GetRelacionIngresosMaestroResult)
-         vm.url = $sce.trustAsResourceUrl(globalService.getUrlReportes()+"/Reportes/"+result.GetRelacionIngresosMaestroResult);
+        vm.url = $sce.trustAsResourceUrl(globalService.getUrlReportes()+"/Reportes/"+result.GetRelacionIngresosMaestroResult);
       });
     }
 
