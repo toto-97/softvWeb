566507f710d2e4d4dd03fb885092f0d8a26a7fb8
unknown <jano_ea@hotmail.com>
2018-04-05 16:19:28 -0500

    Correción CAMDO

--- app/scripts/global.js
+++ app/scripts/global.js
@@ -8,8 +8,8 @@ angular
 		//rutas locales
 		 svc.getUrl = function() {
 			//return 'http://172.16.126.44:81/SoftvWCFService.svc';
-		   	return 'http://192.168.50.33/SoftvWCFService.svc';
-	  		//return 'http://localhost:64481/SoftvWCFService.svc';
+		   	//return 'http://192.168.50.33/SoftvWCFService.svc';
+	  		return 'http://localhost:64481/SoftvWCFService.svc';
 		};
 
 		svc.getUrlReportes = function() {
